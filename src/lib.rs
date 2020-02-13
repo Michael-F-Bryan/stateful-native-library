35de4f541dd79c53825f351227f4f34908c7ab9d
Michael Bryan <michaelfbryan@gmail.com>
2020-02-13 21:53:59 +0800

    Stubbed out the type-level state machine for working with the library

---
+++ src/lib.rs
@@ -0,0 +1,134 @@
+use std::{
+    marker::PhantomData,
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+pub fn execute<P>(_recipe: Recipe<'_>, _progress: P) -> Result<Output, Error>
+where
+    P: FnMut(i32),
+{
+    unimplemented!()
+}
+
+/// A handle to the `stateful` library.
+pub struct Library {
+    _not_send: PhantomData<*const ()>,
+}
+
+static LIBRARY_IN_USE: AtomicBool = AtomicBool::new(false);
+
+impl Library {
+    pub fn new() -> Result<Library, AlreadyInUse> {
+        if LIBRARY_IN_USE.compare_and_swap(false, true, Ordering::SeqCst)
+            == false
+        {
+            Ok(Library {
+                _not_send: PhantomData,
+            })
+        } else {
+            Err(AlreadyInUse)
+        }
+    }
+
+    pub fn set_parameters(&mut self) -> SettingParameters<'_> {
+        SettingParameters { _library: self }
+    }
+
+    pub fn create_recipe(&mut self) -> RecipeBuilder<'_> {
+        RecipeBuilder { _library: self }
+    }
+}
+
+impl Drop for Library {
+    fn drop(&mut self) { LIBRARY_IN_USE.store(false, Ordering::SeqCst); }
+}
+
+/// An error indicating the `Library` can't be accessed again until the existing
+/// session is over.
+#[derive(Debug, Copy, Clone, PartialEq, thiserror::Error)]
+#[error("The library is already in use")]
+pub struct AlreadyInUse;
+
+pub struct SettingParameters<'lib> {
+    _library: &'lib mut Library,
+}
+
+impl<'lib> SettingParameters<'lib> {
+    pub fn boolean(&mut self, _name: &str, _value: bool) -> &mut Self {
+        unimplemented!()
+    }
+
+    pub fn integer(&mut self, _name: &str, _value: i32) -> &mut Self {
+        unimplemented!()
+    }
+}
+
+pub struct RecipeBuilder<'lib> {
+    _library: &'lib mut Library,
+}
+
+impl<'lib> RecipeBuilder<'lib> {
+    pub fn add_item(&mut self, _name: &str, _value: i32) -> &mut Self {
+        unimplemented!()
+    }
+
+    pub fn add_group<'r>(&'r mut self, _name: &str) -> GroupBuilder<'r, 'lib> {
+        GroupBuilder {
+            _recipe_builder: self,
+        }
+    }
+
+    pub fn build(self) -> Recipe<'lib> {
+        Recipe {
+            _library: self._library,
+        }
+    }
+}
+
+pub struct GroupBuilder<'r, 'lib> {
+    _recipe_builder: &'r mut RecipeBuilder<'lib>,
+}
+
+impl<'r, 'lib> GroupBuilder<'r, 'lib> {
+    pub fn add_item(&mut self, _name: &str, _value: i32) -> &mut Self {
+        unimplemented!()
+    }
+
+    pub fn finish(self) -> &'r mut RecipeBuilder<'lib> { self._recipe_builder }
+}
+
+pub struct Recipe<'lib> {
+    _library: &'lib mut Library,
+}
+
+pub enum Error {}
+
+pub struct Output {
+    pub items: Vec<i32>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static_assertions::assert_not_impl_any!(Library: Send, Sync);
+
+    #[test]
+    fn cant_create_multiple_library_handles_at_the_same_time() {
+        let first_library = Library::new().unwrap();
+
+        // make sure the flag is set
+        assert!(LIBRARY_IN_USE.load(Ordering::SeqCst));
+
+        // then try to create another handle
+        assert!(Library::new().is_err());
+
+        // explicitly drop the first library so we know it clears the flag
+        drop(first_library);
+
+        assert!(!LIBRARY_IN_USE.load(Ordering::SeqCst));
+
+        // now the old handle is destroyed, we can create another
+        let _another = Library::new().unwrap();
+    }
+}
