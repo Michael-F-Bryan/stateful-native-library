f5ba07fd3e80afb75e3db04d0109e4760011b840
Michael Bryan <michaelfbryan@gmail.com>
2020-02-15 15:36:57 +0800

    Implemented execute and filled in any unimplemented!() methods

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,90 @@ mod bindings;
 
 use std::{
     convert::TryFrom,
+    ffi::CString,
     marker::PhantomData,
-    os::raw::c_int,
+    mem::ManuallyDrop,
+    os::raw::{c_int, c_void},
+    ptr,
     sync::atomic::{AtomicBool, Ordering},
 };
 
-pub fn execute<P>(_recipe: Recipe<'_>, _progress: P) -> Result<Output, Error>
+const NUL_MSG: &str = "valid names shouldn't contain null characters";
+
+pub fn execute<P>(_recipe: Recipe<'_>, mut progress: P) -> Result<Output, Error>
 where
     P: FnMut(i32),
 {
-    unimplemented!()
+    // Safety: Accepting a `Recipe` means we prove at compile time that setting
+    // these variables can't result in any data races.
+    static mut ON_PROGRESS_USER_DATA: *mut c_void = ptr::null_mut();
+    static mut TEMPORARY_RESULT: Option<Output> = None;
+
+    unsafe extern "C" fn on_progress<F>(percent: c_int) -> c_int
+    where
+        F: FnMut(i32),
+    {
+        // Safety: This requires us to store a pointer to `progress` when
+        // `execute()` is called and make sure the `F` type variable
+        // `on_progress()` is instantiated with is the same as `execute()`'s `P`
+        let actual_progress_callback = &mut *(ON_PROGRESS_USER_DATA as *mut F);
+
+        actual_progress_callback(percent);
+        bindings::RESULT_OK as c_int
+    }
+
+    unsafe extern "C" fn on_finished(_num_items: c_int) -> c_int {
+        let mut output = Output::default();
+
+        let mut item = 0;
+
+        while bindings::stateful_get_output_by_index(
+            output.items.len() as c_int,
+            &mut item,
+        ) == bindings::RESULT_OK as c_int
+        {
+            output.items.push(item);
+        }
+
+        // Safety: Accepting a `Recipe` means this can only be set by one thread
+        // at a time
+        TEMPORARY_RESULT = Some(output);
+
+        bindings::RESULT_OK as c_int
+    }
+
+    unsafe {
+        // Safety: Accepting a `Recipe` means this can only be set by one thread
+        // at a time
+        ON_PROGRESS_USER_DATA = &mut progress as *mut P as *mut c_void;
+
+        let ret = bindings::stateful_execute(
+            Some(on_progress::<P>),
+            Some(on_finished),
+        )
+        .into_result();
+
+        // We need to take the temporary result before handling
+        // stateful_execute()'s return code so we don't leak an `Output`.
+        let output = TEMPORARY_RESULT.take();
+
+        // just bail if something went wrong
+        if let Err(e) = ret {
+            return Err(e);
+        }
+
+        // We need to make sure we actually set the temporary result. The only
+        // way this could possibly happen is if `stateful_execute()` ran to
+        // completion and said it finished successfully without actually
+        // invoking our `on_finished` callback... If so, that's a programming
+        // error in the underlying library and nothing the caller can reasonably
+        // be expected to handle
+
+        match output {
+            Some(output) => Ok(output),
+            None => panic!("The stateful_execute function said it returned successfully without calling our on_finished callback. This is a bug.")
+        }
+    }
 }
 
 /// A handle to the `stateful` library.
@@ -95,7 +169,9 @@ impl Library {
     /// ```
     pub fn create_recipe(&mut self) -> RecipeBuilder<'_> {
         cant_fail!(unsafe { bindings::stateful_start_adding_items() });
-        RecipeBuilder { _library: self }
+        RecipeBuilder {
+            _library: ManuallyDrop::new(self),
+        }
     }
 }
 
@@ -113,34 +189,70 @@ pub struct SettingParameters<'lib> {
 }
 
 impl<'lib> SettingParameters<'lib> {
-    pub fn boolean(&mut self, _name: &str, _value: bool) -> &mut Self {
-        unimplemented!()
+    pub fn boolean(&mut self, name: &str, value: bool) -> &mut Self {
+        let name = CString::new(name).expect(NUL_MSG);
+
+        unsafe {
+            cant_fail!(bindings::stateful_set_bool_var(name.as_ptr(), value));
+        }
+
+        self
     }
 
-    pub fn integer(&mut self, _name: &str, _value: i32) -> &mut Self {
-        unimplemented!()
+    pub fn integer(&mut self, name: &str, value: i32) -> &mut Self {
+        let name = CString::new(name).expect(NUL_MSG);
+
+        unsafe {
+            cant_fail!(bindings::stateful_set_int_var(name.as_ptr(), value));
+        }
+
+        self
+    }
+}
+
+impl<'lib> Drop for SettingParameters<'lib> {
+    fn drop(&mut self) {
+        unsafe {
+            let _ = bindings::stateful_end_setting_parameters().into_result();
+        }
     }
 }
 
 pub struct RecipeBuilder<'lib> {
-    _library: &'lib mut Library,
+    // Note: We use ManuallyDrop::take() so we can move the `&mut Library`
+    // reference out of `RecipeBuilder` even though it has a Drop impl.
+    //
+    // This is okay because a mutable reference doesn't need to do anything
+    // special when it goes out of scope
+    //
+    // https://users.rust-lang.org/t/moving-out-of-a-type-implementing-drop/38225/5?u=michael-f-bryan
+    _library: ManuallyDrop<&'lib mut Library>,
 }
 
 impl<'lib> RecipeBuilder<'lib> {
-    pub fn add_item(&mut self, _name: &str, _value: i32) -> &mut Self {
-        unimplemented!()
+    pub fn add_item(&mut self, name: &str, value: i32) -> &mut Self {
+        let name = CString::new(name).expect(NUL_MSG);
+        cant_fail!(unsafe {
+            bindings::stateful_add_item(name.as_ptr(), value)
+        });
+
+        self
     }
 
-    pub fn add_group<'r>(&'r mut self, _name: &str) -> GroupBuilder<'r, 'lib> {
+    pub fn add_group<'r>(&'r mut self, name: &str) -> GroupBuilder<'r, 'lib> {
+        let name = CString::new(name).expect(NUL_MSG);
+        cant_fail!(unsafe {
+            bindings::stateful_start_adding_group(name.as_ptr())
+        });
         GroupBuilder {
-            _recipe_builder: self,
+            _recipe_builder: ManuallyDrop::new(self),
         }
     }
 
-    pub fn build(self) -> Recipe<'lib> {
-        Recipe {
-            _library: self._library,
-        }
+    pub fn build(mut self) -> Recipe<'lib> {
+        // Safety: RecipeBuilder's Drop impl does not use this field
+        let _library = unsafe { ManuallyDrop::take(&mut self._library) };
+        Recipe { _library }
     }
 }
 
@@ -153,15 +265,25 @@ impl<'lib> Drop for RecipeBuilder<'lib> {
 }
 
 pub struct GroupBuilder<'r, 'lib> {
-    _recipe_builder: &'r mut RecipeBuilder<'lib>,
+    _recipe_builder: ManuallyDrop<&'r mut RecipeBuilder<'lib>>,
 }
 
 impl<'r, 'lib> GroupBuilder<'r, 'lib> {
-    pub fn add_item(&mut self, _name: &str, _value: i32) -> &mut Self {
-        unimplemented!()
+    pub fn add_item(&mut self, name: &str, value: i32) -> &mut Self {
+        let name = CString::new(name).expect(NUL_MSG);
+        cant_fail!(unsafe {
+            bindings::stateful_add_group_item(name.as_ptr(), value)
+        });
+
+        self
     }
 
-    pub fn finish(self) -> &'r mut RecipeBuilder<'lib> { self._recipe_builder }
+    pub fn finish(mut self) -> &'r mut RecipeBuilder<'lib> {
+        unsafe {
+            // Safety: GroupBuilder's Drop impl does not use this field
+            ManuallyDrop::take(&mut self._recipe_builder)
+        }
+    }
 }
 
 impl<'r, 'lib> Drop for GroupBuilder<'r, 'lib> {
@@ -189,6 +311,7 @@ pub enum Error {
     Other(c_int),
 }
 
+#[derive(Debug, Default, Clone, PartialEq)]
 pub struct Output {
     pub items: Vec<i32>,
 }
