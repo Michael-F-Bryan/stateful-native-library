346586f90d71a150eab9d163facf3b366b65e077
Michael Bryan <michaelfbryan@gmail.com>
2020-02-17 22:47:26 +0800

    Added comments to the example

--- examples/basic-usage.rs
+++ examples/basic-usage.rs
@@ -6,14 +6,17 @@ use stateful_native_library::{Error, Library};
 fn main() -> Result<(), Error> {
     let mut library = Library::new()?;
 
+    // set some parameters
     library
         .set_parameters()
         .boolean("foo", false)
         .integer("some-number", 42);
 
+    // start building the recipe
     let mut recipe_builder = library.create_recipe();
     recipe_builder.add_item("first", 1).add_item("second", 2);
 
+    // we can add several groups using a loop
     for i in 0..5 {
         let name = format!("group_{}", i);
         let mut group_builder = recipe_builder.add_group(&name);
@@ -26,14 +29,16 @@ fn main() -> Result<(), Error> {
         group_builder.finish();
     }
 
-    let mut another_group_builder = recipe_builder.add_group("another group");
-    another_group_builder
+    // or use normal builder methods
+    recipe_builder
+        .add_group("another group")
         .add_item("another nested item", 5)
         .add_item("MOAR items", 6);
-    another_group_builder.finish();
 
+    // finish building the recipe
     let recipe = recipe_builder.build();
 
+    // then get the outcome, periodically printing out progress messages
     let outcome = stateful_native_library::execute(recipe, |percent| {
         println!("{}%", percent)
     })?;
